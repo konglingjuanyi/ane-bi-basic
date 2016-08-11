5b7c90086f0dc69389a51be83f6acedab25a6ddd
yangdechao <yangdechao@ane56.com>
2016-08-11 17:51:08 +0800

    submit source

--- src/main/webapp-src/src/server/route/data.js
+++ src/main/webapp-src/src/server/route/data.js
@@ -1,17 +1,111 @@
 module.exports = {
-	getUsers: getUsers,
-	getUsers2 : getUsers2 ,
+	getUsers : getUsers,
+	getUsers2 : getUsers2,
 	getUser : getUser,
 };
 
-function getUser(){
-	return {"status":"SUCCESS","result":{"id":235957252521984,"username":"abc","password":"123456","nikename":"abc1234567","created":1468252800000,"status":true}};
+function getUser() {
+	return {
+		"status" : "SUCCESS",
+		"result" : {
+			"id" : 235957252521984,
+			"username" : "abc",
+			"password" : "123456",
+			"nikename" : "abc1234567",
+			"created" : 1468252800000,
+			"status" : true
+		}
+	};
 }
 
-function getUsers(){
-	return {"status":"SUCCESS","result":[{"id":235957252521984,"username":"abc","password":"123456","nikename":"abc1234567","created":1468252800000,"status":true},{"id":235969623097344,"username":"abc","password":"123456","nikename":"abc1234567","created":1468252800000,"status":true},{"id":235974638567424,"username":"abc","password":"123456","nikename":"abc1234567","created":1468252800000,"status":true},{"id":235979888394240,"username":"abc","password":"123456","nikename":"abc1234567","created":1468252800000,"status":true},{"id":236086589259776,"username":"abc","password":"123456","nikename":"abc1234567","created":1468252800000,"status":true},{"id":236111474262016,"username":"abc","password":"123456","nikename":"abc1234567","created":1468252800000,"status":true}]};
+function getUsers() {
+	return {
+		"status" : "SUCCESS",
+		"result" : [ {
+			"id" : 235957252521984,
+			"username" : "abc",
+			"password" : "123456",
+			"nikename" : "abc1234567",
+			"created" : 1468252800000,
+			"status" : true
+		}, {
+			"id" : 235969623097344,
+			"username" : "abc",
+			"password" : "123456",
+			"nikename" : "abc1234567",
+			"created" : 1468252800000,
+			"status" : true
+		}, {
+			"id" : 235974638567424,
+			"username" : "abc",
+			"password" : "123456",
+			"nikename" : "abc1234567",
+			"created" : 1468252800000,
+			"status" : true
+		}, {
+			"id" : 235979888394240,
+			"username" : "abc",
+			"password" : "123456",
+			"nikename" : "abc1234567",
+			"created" : 1468252800000,
+			"status" : true
+		}, {
+			"id" : 236086589259776,
+			"username" : "abc",
+			"password" : "123456",
+			"nikename" : "abc1234567",
+			"created" : 1468252800000,
+			"status" : true
+		}, {
+			"id" : 236111474262016,
+			"username" : "abc",
+			"password" : "123456",
+			"nikename" : "abc1234567",
+			"created" : 1468252800000,
+			"status" : true
+		} ]
+	};
 }
 
-function getUsers2(){
-	return {"status":"SUCCESS","result":{"result":[{"id":235974638567424,"username":"abc","password":"123456","nikename":"abc1234567","created":1468252800000,"status":true},{"id":235979888394240,"username":"abc","password":"123456","nikename":"abc1234567","created":1468252800000,"status":true}],"total":7,"offset":2,"limit":2,"current":2}};
+function getUsers2() {
+	return {
+		"status" : "SUCCESS",
+		"result" : {
+			"result" : [ {
+				"id" : 235974638567424,
+				"username" : "abc",
+				"password" : "123456",
+				"nikename" : "abc1234567",
+				"created" : 1468252800000,
+				"status" : true
+			}, {
+				"id" : 2335979888394240,
+				"username" : "abc",
+				"password" : "123456",
+				"nikename" : "abc1234567",
+				"created" : 1468252800000,
+				"status" : true
+			} ,
+			{
+				"id" : 235979888394240,
+				"username" : "abc",
+				"password" : "123456",
+				"nikename" : "abc1234567",
+				"created" : 1468252800000,
+				"status" : true
+			},
+			{
+				"id" : 235979888394240,
+				"username" : "abc",
+				"password" : "123456",
+				"nikename" : "abc1234567",
+				"created" : 1468252800000,
+				"status" : true
+			}],
+			"total" : 7,
+			"offset" : 2,
+			"limit" : 2,
+			"current" : 2
+		}
+	};
 }

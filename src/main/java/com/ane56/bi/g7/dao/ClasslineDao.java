19c4e0ba24f287190e9fd71891a041d200945729
yangdechao <yangdechao@ane56.com>
2016-08-29 10:42:57 +0800

    班线同步接口优化

--- src/main/java/com/ane56/bi/g7/dao/ClasslineDao.java
+++ src/main/java/com/ane56/bi/g7/dao/ClasslineDao.java
@@ -3,8 +3,6 @@ import java.sql.SQLException;
 import java.util.Date;
 import java.util.Map;
 
-import oracle.sql.DATE;
-
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.MapHandler;
 
@@ -12,33 +10,79 @@ import com.ane56.bi.common.util.CommonUtils;
 import com.ane56.bi.common.util.DateUtils;
 import com.ane56.bi.common.util.TxQueryRunner;
 import com.ane56.bi.g7.domain.Classline;
+import com.ane56.bi.g7.domain.PassInfoData;
 import com.ane56.bi.g7.domain.Test;
-import com.sun.org.apache.bcel.internal.generic.NEW;
 public class ClasslineDao {
 	private QueryRunner qr = new TxQueryRunner();
 	
 
 	
 	/**
-	 * 按bid查询
-	 * @param bid
+	 * 按id查询班线信息
+	 * @param id
 	 * @return
 	 * @throws SQLException
 	 */
-	public Classline findByBid(String bid) throws SQLException {
+	public Classline findClassLineByBid(String id) throws SQLException {
+		Classline classline = null;
 		String sql = "SELECT * FROM ODS_BDP.ANE_BI_BASIC_CLASSLINE b WHERE b.\"id\"=?";
-		// 一行记录中，包含了很多的Classline的属性，还有一个cid属性
-		Map<String,Object> map = qr.query(sql, new MapHandler(), bid);
-		// 把Map中除了cid以外的其他属性映射到Classline对象中
-		Classline classline = CommonUtils.toBean(map, Classline.class);
-		return classline;
+		// 一行记录中，包含了很多的Classline的属性，还有一个id属性
+		Map<String,Object> map = qr.query(sql, new MapHandler(), id);
+		if(map == null){
+			return classline;
+		}else{
+			// 把Map中除了id以外的其他属性映射到Classline对象中
+			classline = CommonUtils.toBean(map, Classline.class);
+			return classline;
+		}
 	}
+	/**
+	 * 添加passInfo
+	 * @param book
+	 * @throws SQLException 
+	 */
+	public boolean addPassInfoData(PassInfoData passInfo) throws SQLException {
+		String sql = "INSERT INTO ODS_BDP.ANE_BI_BASIC_PASSINFO a (a.\"id\","+
+		"a.\"orgroot\", a.\"lineid\", a.\"siteid\", a.\"order\", a.\"runtime\","+
+		"a.\"staytime\", a.\"szflag\", a.\"tomiles\", a.\"name\", a.\"lat\", a.\"lng\", a.\"passSiteLngLat\")"+
+				" values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+		Object[] params = {passInfo.getId(),passInfo.getOrgroot(),passInfo.getLineid(),passInfo.getSiteid(),
+				passInfo.getOrder(),passInfo.getRuntime(),passInfo.getStaytime(),passInfo.getSzflag(),passInfo.getTomiles(),
+				passInfo.getName(),passInfo.getLat(),passInfo.getLng(),passInfo.getPassSiteLngLat()};
+		int result = qr.update(sql, params);
+		if(result==1){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	/**
+	 * 按id查询passInfo
+	 * @param id
+	 * @return
+	 * @throws SQLException
+	 */
+	public PassInfoData findPassInfoByBid(String id) throws SQLException {
+		PassInfoData passInfo = null;
+		String sql = "SELECT * FROM ODS_BDP.ANE_BI_BASIC_PASSINFO b WHERE b.\"id\"=?";
+		// 一行记录中，包含了很多的Classline的属性，还有一个id属性
+		Map<String,Object> map = qr.query(sql, new MapHandler(), id);
+		if(map==null){
+			return passInfo;
+		}else{
+			// 把Map中除了cid以外的其他属性映射到Classline对象中
+			passInfo = CommonUtils.toBean(map, PassInfoData.class);
+			return passInfo;
+		}
+	}
+	
 	/**
 	 * 添加班线
 	 * @param book
 	 * @throws SQLException 
 	 */
-	public void addClassLine(Classline line) throws SQLException {
+	public boolean addClassLine(Classline line) throws SQLException {
 		String sql = "INSERT INTO ODS_BDP.ANE_BI_BASIC_CLASSLINE a (a.\"id\","+
 	    "a.\"orgroot\", a.\"orgcode\", a.\"orgroot_name\", a.\"orgcode_name\", a.\"deleted\", a.\"code\", a.\"name\","+
 		"a.\"startsite\", a.\"endsite\", a.\"startsiteid\", a.\"endsiteid\", a.\"runtime\", a.\"totalmileage\", a.\"startsitecode\"," +
@@ -47,9 +91,13 @@ public class ClasslineDao {
 		Object[] params = {line.getId(),line.getOrgroot(),line.getOrgcode(),line.getOrgroot_name(),line.getOrgcode_name(),line.getDeleted(),
 				line.getCode(),line.getName(),line.getStartsite(),line.getEndsite(),line.getStartsiteid(),line.getEndsiteid(),line.getRuntime(),
 				line.getTotalmileage(),line.getStartsitecode(),line.getEndsitecode(),line.getIs_share(),line.getStartSiteLngLat(),line.getEndSiteLngLat()};
-		qr.update(sql, params);
+		int result = qr.update(sql, params);
+		if(result==1){
+			return true;
+		}else{
+			return false;
+		}
 	}
-	
 	public void addTest(Test test){
 		/*String sql = "insert into ODS_BDP.TEST b (b.id,b.name,b.age,b.birthday,b.money)"  +
 				" values(?,?,?,to_date(?,'SYYYY-MM-DD HH24:MI:SS'),?)";*/
--- src/main/java/com/ane56/bi/g7/service/ClasslineService.java
+++ src/main/java/com/ane56/bi/g7/service/ClasslineService.java
@@ -1,42 +1,64 @@
 package com.ane56.bi.g7.service;
+
 import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import com.ane56.bi.g7.dao.ClasslineDao;
 import com.ane56.bi.g7.domain.Classline;
 import com.ane56.bi.g7.domain.PageResult;
+import com.ane56.bi.g7.domain.PassInfoData;
 import com.ane56.bi.g7.domain.Test;
 
 public class ClasslineService {
 	private ClasslineDao classlineDao = new ClasslineDao();
-	
-	public Classline findByBid(String cid) {
+
+	public Classline findByBid(String id) {
 		try {
-			return classlineDao.findByBid(cid);
-		} catch(SQLException e) {
+			return classlineDao.findClassLineByBid(id);
+		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	 public void addTest(Test test){
-		 classlineDao.addTest(test);
-	 }
-	
 
-	 public void addClassLine(PageResult pageResult) throws Exception{
-		 if(pageResult!=null){
-			 List<Classline> classLine = pageResult.getResult();
-			 if(!CollectionUtils.isEmpty(classLine)){
-				 for(int i=0;i<classLine.size();i++){
-					 Classline g7bean = classLine.get(i);
-						classlineDao.addClassLine(g7bean);
-				 }
-				
-				 
-			 }
-		 }
-		 
-	 }
+	public void addTest(Test test) {
+		classlineDao.addTest(test);
+	}
+
+	public boolean addClassLine(PageResult pageResult) throws Exception {
+		if (pageResult != null) {
+			List<Classline> classLine = pageResult.getResult();
+			if (!CollectionUtils.isEmpty(classLine)) {
+				for (int i = 0; i < classLine.size(); i++) {
+					Classline classLineBean = null;
+					Classline g7bean = classLine.get(i);
+					List<PassInfoData> passInfoList = g7bean.getPassinfoList();
+					String cid = g7bean.getId();
+					if (!StringUtils.isEmpty(cid)) {
+						classLineBean = classlineDao.findClassLineByBid(cid);
+						if (classLineBean == null) {
+							classlineDao.addClassLine(g7bean);
+						}
+					}
+					if (!CollectionUtils.isEmpty(passInfoList)) {
+						for (int j = 0; j < passInfoList.size(); j++) {
+							PassInfoData passInfoBean = null;
+							PassInfoData passInfo = passInfoList.get(j);
+							String pid = passInfo.getId();
+							if (!StringUtils.isEmpty(pid)) {
+								passInfoBean = classlineDao.findPassInfoByBid(pid);
+								if (passInfoBean == null) {
+									classlineDao.addPassInfoData(passInfo);
+								}
+							}
+						}
+
+					}
+				}
+			}
+		}
+		return true;
+	}
 }

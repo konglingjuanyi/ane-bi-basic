f8a5dbb8bb5da43e0c67bd7096d07779f0adec0b
ydch10086 <438803719@qq.com>
2016-09-26 13:12:01 +0800

    修改

--- src/main/java/com/ane56/bi/application/PlanTimeService.java
+++ src/main/java/com/ane56/bi/application/PlanTimeService.java
@@ -6,35 +6,35 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.ane56.bi.common.pager.PageBean;
 import com.ane56.bi.domain.AssertionConcern;
-import com.ane56.bi.domain.operation.PlanTime;
-import com.ane56.bi.domain.operation.PlanTimeRepository;
+import com.ane56.bi.domain.operation.BdpDstrbPlanOpTime;
+import com.ane56.bi.domain.operation.BdpDstrbPlanOpTimeRepository;
 
 @Service
-public class PlanTimeService extends AssertionConcern {
+public class BdpDstrbPlanOpTimeService extends AssertionConcern {
 
 	@Autowired
-	private PlanTimeRepository planTimeRepository;
+	private BdpDstrbPlanOpTimeRepository BdpDstrbPlanOpTimeRepository;
 	
 
 	@Transactional
-	public int addPlanTime(PlanTime data) {
-		return planTimeRepository.add(data);
+	public int addBdpDstrbPlanOpTime(BdpDstrbPlanOpTime data) {
+		return BdpDstrbPlanOpTimeRepository.add(data);
 	}
 
-   public List<PlanTime> findByParams(Map<String,Object> condition){
-	   List<PlanTime> result = null;
-	   result = planTimeRepository.findByParams(condition);
+   public List<BdpDstrbPlanOpTime> findByParams(Map<String,Object> condition){
+	   List<BdpDstrbPlanOpTime> result = null;
+	   result = BdpDstrbPlanOpTimeRepository.findByParams(condition);
 	   return result;
    }
    @Transactional
-   public int updatePlanTime(PlanTime data){
-	   int result = planTimeRepository.update(data);
+   public int updateBdpDstrbPlanOpTime(BdpDstrbPlanOpTime data){
+	   int result = BdpDstrbPlanOpTimeRepository.update(data);
 	   return result;
    }
 
-   public PageBean<PlanTime> queryDataByPage(Map<String,Object> paramObject,int offset, int limit){
-	   PageBean<PlanTime> result = null;
-	   result = planTimeRepository.queryDataByPage(paramObject,offset,limit);
+   public PageBean<BdpDstrbPlanOpTime> queryDataByPage(Map<String,Object> paramObject,int offset, int limit){
+	   PageBean<BdpDstrbPlanOpTime> result = null;
+	   result = BdpDstrbPlanOpTimeRepository.queryDataByPage(paramObject,offset,limit);
 	   return result;
    }
    
